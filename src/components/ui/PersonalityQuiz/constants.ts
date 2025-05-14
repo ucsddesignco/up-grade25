044fec1fdc874198602b9f9817d181650b5e0771
Shamita <goyal.shamita4@gmail.com>
2025-05-14 14:21:37 -0700

    added initial UI design, the questions, and the back and continue button

---
+++ src/components/ui/PersonalityQuiz/constants.ts
@@ -0,0 +1,61 @@
+export const QuizQuestions = [
+    {
+        id: '1',
+        question: "Which part of creating a digital product is the most exciting to you?",
+        option: [
+            {id:'a', text: "Bringing designs to life through code"},
+            {id:'b', text: "Mapping out intuitive user flows"},
+            {id:'c', text: "Crafting the look and feel"},
+            {id:'d', text: "Keeping the team organized"},
+            {id:'e', text: "Shaping the message and content "}
+        ]
+    },
+
+    {
+        id: '2',
+        question: 'What do you prefer when it comes to design?',
+        option: [
+            {id:'a', text: "Minimalism"},
+            {id: 'b', text: "Bold and colorful designs"},
+            {id: 'c', text: "Retro vibes"},
+            {id: 'd', text: "Sleek and modern aesthetics"},
+            {id: 'e', text: "Whimsical and playful"}
+        ]
+    },
+
+    {
+        id: '3',
+        question: 'What is your go-to method for solving a problem?',
+        option: [
+            {id:'a', text: "Brainstorming with others"},
+            {id: 'b', text: "Researching the issue deeply"},
+            {id: 'c', text: "Testing different approaches"},
+            {id: 'd', text: "Breaking the problem into smaller pieces"},
+            {id: 'e', text: "Trusting my gut instinct"}
+        ]
+    },
+
+    {
+        id: '4',
+        question: 'How do you feel about collaboration in a project?',
+        option: [
+            {id:'a', text: "I love working with a team and sharing ideas"},
+            {id: 'b', text: "I prefer working alone to maintain control"},
+            {id: 'c', text: "I’m comfortable in both settings"},
+            {id: 'd', text: "I prefer to lead the team"},
+            {id: 'e', text: "I’m flexible depending on the project"}
+        ]
+    },
+
+    {
+        id: '5',
+        question: 'What’s your ideal work environment?',
+        option: [
+            {id:'a', text: "A creative and collaborative space"},
+            {id: 'b', text: "A quiet and focused environment"},
+            {id: 'c', text: "A dynamic and fast-paced office"},
+            {id: 'd', text: "A remote and flexible work setup"},
+            {id: 'e', text: "A balance of quiet and collaboration"}
+        ]
+    },
+];
