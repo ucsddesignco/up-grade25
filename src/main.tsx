315b5bba46eb75dd256b20bf46732af0964e0e93
EdwardNew <pkarrows@gmail.com>
2025-05-19 15:23:48 -0700

    fix: fix linting errors

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,12 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Quiz from "./pages/Quiz/Quiz.tsx";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/quiz" element={<Quiz />} />
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 );
