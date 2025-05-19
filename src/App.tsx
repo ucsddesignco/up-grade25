5b6a37059ae3ebd3204e8cf0b7ec1c1f4b59ee13
EdwardNew <pkarrows@gmail.com>
2025-05-19 15:21:03 -0700

    feat: added keen slider for pegboard and added dropdown menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import "./App.css";
 import Pegboard from "./components/ui/Pegboard/Pegboard";
+import Dropdown from "./components/ui/Dropdown/Dropdown";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import { useState, useEffect } from "react";
+
+const slideColors = [
+    "#FFF2DC",
+    "#FEE8EB",
+    "#E7F3DD",
+    "#DCEEFF",
+    "#FFE8DC",
+    "#E2F5F9",
+];
 
 function App() {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [sliderRef, instanceRef] = useKeenSlider({
+        slideChanged() {
+            console.log("slide changed");
+        },
+    });
+
+    useEffect(() => {
+        if (instanceRef.current) {
+            instanceRef.current.moveToIdx(selectedIndex);
+        }
+    }, [selectedIndex, instanceRef]);
+
     return (
         <>
-            <div>
-                <Pegboard color="#FFF2DC" />
+            <Dropdown
+                selected={selectedIndex + 1}
+                onChange={(val) => setSelectedIndex(val - 1)}
+            />
+            <div ref={sliderRef} className="keen-slider">
+                {slideColors.map((color, idx) => (
+                    <div key={idx} className="keen-slider__slide">
+                        <Pegboard color={color} />
+                    </div>
+                ))}
             </div>
         </>
     );
