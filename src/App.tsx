f5e34e33ccc555299ae0db82f56a9bdbad93278d
EdwardNew <pkarrows@gmail.com>
2025-05-19 16:48:21 -0700

    fixed pegboard slide numbers

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,48 @@ import Pegboard from "./components/ui/Pegboard/Pegboard";
 import Dropdown from "./components/ui/Dropdown/Dropdown";
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const slideColors = [
-  "#FFF2DC",
-  "#FEE8EB",
-  "#E7F3DD",
-  "#DCEEFF",
-  "#FFE8DC",
-  "#E2F5F9",
+    "#FFF2DC",
+    "#FEE8EB",
+    "#E7F3DD",
+    "#DCEEFF",
+    "#FFE8DC",
+    "#E2F5F9",
 ];
 import "./App.css";
 
 function App() {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [sliderRef, instanceRef] = useKeenSlider({
-    slideChanged() {
-      console.log("slide changed");
-    },
-  });
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [sliderRef, instanceRef] = useKeenSlider({
+        loop: true,
+        slideChanged() {
+            const currentIndex = instanceRef.current?.track.details.rel;
+            if (currentIndex !== undefined) {
+                setSelectedIndex(currentIndex);
+            }
+        },
+    });
 
-  useEffect(() => {
-    if (instanceRef.current) {
-      instanceRef.current.moveToIdx(selectedIndex);
-    }
-  }, [selectedIndex, instanceRef]);
-
-  return (
-    <>
-      <Dropdown
-        selected={selectedIndex + 1}
-        onChange={(val) => setSelectedIndex(val - 1)}
-      />
-      <div ref={sliderRef} className="keen-slider">
-        {slideColors.map((color, idx) => (
-          <div key={idx} className="keen-slider__slide">
-            <Pegboard color={color} />
-          </div>
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Dropdown
+                selected={selectedIndex}
+                onChange={(val) => {
+                    setSelectedIndex(val);
+                    instanceRef.current?.moveToIdx(val);
+                }}
+            />
+            <div ref={sliderRef} className="keen-slider">
+                {slideColors.map((color, idx) => (
+                    <div key={"pegboard" + idx} className="keen-slider__slide">
+                        <Pegboard color={color} />
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default App;
