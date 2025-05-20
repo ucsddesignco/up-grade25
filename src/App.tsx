bb88324e68c7a36984968c0cdd627f1aa323da7e
EdwardNew <pkarrows@gmail.com>
2025-05-19 17:34:50 -0700

    fix: fix linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -7,45 +7,45 @@ import { useKeenSlider } from "keen-slider/react";
 import { useState } from "react";
 
 const slideColors = [
-    "#FFF2DC",
-    "#FEE8EB",
-    "#E7F3DD",
-    "#DCEEFF",
-    "#FFE8DC",
-    "#E2F5F9",
+  "#FFF2DC",
+  "#FEE8EB",
+  "#E7F3DD",
+  "#DCEEFF",
+  "#FFE8DC",
+  "#E2F5F9",
 ];
 import "./App.css";
 
 function App() {
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [sliderRef, instanceRef] = useKeenSlider({
-        loop: true,
-        slideChanged() {
-            const currentIndex = instanceRef.current?.track.details.rel;
-            if (currentIndex !== undefined) {
-                setSelectedIndex(currentIndex);
-            }
-        },
-    });
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    loop: true,
+    slideChanged() {
+      const currentIndex = instanceRef.current?.track.details.rel;
+      if (currentIndex !== undefined) {
+        setSelectedIndex(currentIndex);
+      }
+    },
+  });
 
-    return (
-        <>
-            <Dropdown
-                selected={selectedIndex}
-                onChange={(val) => {
-                    setSelectedIndex(val);
-                    instanceRef.current?.moveToIdx(val);
-                }}
-            />
-            <div ref={sliderRef} className="keen-slider">
-                {slideColors.map((color, idx) => (
-                    <div key={"pegboard" + idx} className="keen-slider__slide">
-                        <Pegboard color={color} />
-                    </div>
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Dropdown
+        selected={selectedIndex}
+        onChange={(val) => {
+          setSelectedIndex(val);
+          instanceRef.current?.moveToIdx(val);
+        }}
+      />
+      <div ref={sliderRef} className="keen-slider">
+        {slideColors.map((color, idx) => (
+          <div key={"pegboard" + idx} className="keen-slider__slide">
+            <Pegboard color={color} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -1,24 +1,24 @@
 import "./Dropdown.css";
 
 interface DropdownProps {
-    selected: number;
-    onChange: (value: number) => void;
+  selected: number;
+  onChange: (value: number) => void;
 }
 
 export default function Dropdown({ selected, onChange }: DropdownProps) {
-    return (
-        <div className="dropdown-container">
-            <select
-                className="dropdown-select"
-                value={selected}
-                onChange={(e) => onChange(Number(e.target.value))}
-            >
-                {[...Array(6).keys()].map((num) => (
-                    <option key={num} value={num}>
-                        Slide {num}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+  return (
+    <div className="dropdown-container">
+      <select
+        className="dropdown-select"
+        value={selected}
+        onChange={(e) => onChange(Number(e.target.value))}
+      >
+        {[...Array(6).keys()].map((num) => (
+          <option key={num} value={num}>
+            Slide {num}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
