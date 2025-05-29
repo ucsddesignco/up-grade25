a1fa6ad562b2f0a957c336d11dea5caa48787801
pa1kina <p.volskaya@gmail.com>
2025-05-28 23:00:41 -0700

    tablet configuration for overview page

--- src/pages/Overview/OverviewMobile.tsx
+++ src/pages/Overview/OverviewMobile.tsx
@@ -3,12 +3,14 @@ import "./OverviewMobile.css"
 // import CalendarIcon from "./assets/icons/icon-calendar.svg"
 // import WeeksIcon from "./assets/icons/icon-weeks.svg"
 // import ClockIcon from "./assets/icons/icon-clock.svg"
-import Phone from "../../components/Overview/Phone.tsx"
-import Clock from "../../components/Overview/Clock.tsx"
+import Phone from "../../components/Overview/mobile/Phone.tsx";
+import Clock from "../../components/Overview/mobile/Clock.tsx";
+import Paintbrush from "../../components/Overview/mobile/Paintbrush.tsx";
+import ArrowMeasure from "../../components/Overview/mobile/ArrowMeasure.svg";
+import FaqCard from "../../components/Overview/mobile/FaqCardMobile.tsx";
+import ApplyCard from "../../components/Overview/mobile/ApplyCardMobile.tsx";
+
 import OverviewText from "./OverviewText.json"
-import Paintbrush from "../../components/Overview/Paintbrush.tsx"
-import FaqCard from "../../components/Overview/FaqCard.tsx"
-import ApplyCard from "../../components/Overview/ApplyCard.tsx"
 
 export default function OverviewMobile() {
     return (
@@ -16,42 +18,53 @@ export default function OverviewMobile() {
             <h1 className="mobile-title">Overview</h1>
             <h2 className="mobile-heading1">About</h2>
             <p className="mobile-body">{OverviewText.about}</p>
+
             <h2 className="mobile-heading2">{OverviewText["curious-heading"]}</h2>
             <p className="mobile-body">{OverviewText.curious1}</p>
             <p className="mobile-body">{OverviewText.curious2}</p>
+
             <h2 className="mobile-heading2">Why Should I Apply?</h2>
             <p className="mobile-body">{OverviewText["why-apply"]}</p>
-            <Phone />
+
+            <Phone className="mobile-svg"/>
+
             <h2 className="mobile-heading1">The Task</h2>
             <h2 className="mobile-heading2">{OverviewText["task-heading1"]}</h2>
             <p className="mobile-body">{OverviewText["task-text1"]}</p>
             <h2 className="mobile-heading2">{OverviewText["task-heading2"]}</h2>
             <p className="mobile-body">{OverviewText["task-text2"]}</p>
-            <h2>Not sure what role to try?</h2>
+
+            <h2 style={{ fontSize: '20px', color: '#646676' }}>Not sure what role to try?</h2>
             <button className="quiz-button">TAKE A BRIEF QUIZ</button>
+            <img className="mobile-arrow" src={ArrowMeasure}></img>
+            
+
             <h2 className="mobile-heading2">{OverviewText["build-fast"]}</h2>
             <p className="mobile-body">{OverviewText["build-fast-text"]}</p>
             <h2 className="mobile-heading2">{OverviewText["work-with-industry"]}</h2>
             <p className="mobile-body">{OverviewText["work-with-industry-text"]}</p>
-            <Clock />
+
+            <Clock className="mobile-svg"/>
+
             <h2 className="mobile-heading1">Timeline</h2>
             <p className="mobile-body">{OverviewText["timeline-text"]}</p>
+
             <h2 className="mobile-expectation">{OverviewText.expectation}</h2>
             <div className="mobile-callouts">
-                <div className="team-meetings">
+                <div className="team-meetings callout">
                     <h1>2x</h1>
                     <p>Team Meetings</p>
                 </div>
-                <div className="mentor-meetings">
+                <div className="mentor-meetings callout">
                     <h1>1x</h1>
                     <p>Mentor Meetings</p>
                 </div>
-                <div className="group-workshops">
+                <div className="group-workshops callout">
                     <h1>1x</h1>
                     <p>Group Workshops</p>
                 </div>
             </div>
-            <Paintbrush />
+            <Paintbrush className="mobile-svg"/>
             <FaqCard />
             <ApplyCard />
         </div>
