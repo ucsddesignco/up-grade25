ff40da5793c53e60643fbb9608d5c261ba3bc8f2
wangserene <sew004@ucsd.edu>
2025-05-30 21:03:02 -0700

    EW

--- src/pages/FAQpage/FAQpage.tsx
+++ src/pages/FAQpage/FAQpage.tsx
@@ -1,40 +1,35 @@
 import './FAQstyle.scss';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';;
-
+import * as Accordion from '@radix-ui/react-accordion';
 
 export default function FAQPage() {
     const faqItems = [
       {
         question: 'Who is eligible to participate?',
-        answer: 'Answer goes here'
+        answer: 'All current UCSD students and recent graduates from all majors and experience levels are encouraged to apply! Mentors will provide support in areas where help may be needed, ensuring you have the guidance to succeed.'
       },
       {
         question: 'How many people will be accepted?',
-        answer: 'Answer goes here'
+        answer: 'UP-Grade 2025 will have 6 student teams with ____ participants in each team. Overall, there are ___ participants total.'
       },
       {
         question: 'Can I request teammates?',
-        answer: 'Answer goes here'
+        answer: 'Yes! We will take requests into consideration when assigning participants to teams, however, we cannot guarantee you will be placed with everyone you request.'
       },
       {
         question: 'How will participants be selected?',
-        answer: 'Answer goes here'
+        answer: 'Participants will be selected through a holistic review process that considers their application, past experiences, and interview. We will ensure that candidates are given a fair opportunity to showcase their unique strengths and potential!'
       },
       {
         question: 'Do I need prior design experience?',
-        answer: 'Answer goes here'
+        answer: 'No, prior design experience is not required! We welcome participants of all experience levels– whether you’re a seasoned design pro or just starting your creative career.'
       },
       {
         question: 'Will this program be remote or in-person?',
-        answer: 'Answer goes here'
+        answer: 'This program will be fully remote, allowing participants to join synchonously online.'
       }, 
       {
         question: 'When are applications due?',
-        answer: 'Answer goes here'
+        answer: 'Friday, June 6 at 11:59PM PT. Decisions will be sent out approximately a week after the application deadline.'
       }
   ];
 
@@ -42,26 +37,42 @@ export default function FAQPage() {
     return (
       <>
         <h1>Frequently Asked Questions</h1>
-        {faqItems.map((item, index) => (
-          <Accordion className='overall-accordion'
-            key={index} 
+        <Accordion.Root
+        className="AccordionRoot"
+        type="single"
+        defaultValue="item-1"
+        collapsible
+      >
+        {faqItems.map(item => (
+          <Accordion.Item
+            value={item.answer}
+            key={item.answer}
           >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls={`content-${index}`}
-              id={`content-header-${index}`}
-            >
-              <Typography id='question'>
-                {item.question}
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography id='answer'>
-                {item.answer}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className="AccordionQuestion">
+                <p>{item.question}</p>
+                <svg
+                  className="icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M14 8H8L8 14H6L6 8H0L0 6H6V0L8 0V6L14 6V8Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <p>{item.answer}</p>
+            </Accordion.Content>
+          </Accordion.Item>
         ))}
+      </Accordion.Root>
       </>
     );
 
