5fca0f55680523d0af4f8d95646278dd70458478
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2025-06-04 16:05:42 -0700

    fix: fix photo component anchor point

--- src/components/ui/Pegboard/components/PhotoTeal.tsx
+++ src/components/ui/Pegboard/components/PhotoTeal.tsx
@@ -5,133 +5,132 @@ interface PhotoTealProps extends SVGProps<SVGSVGElement> {
 }
 
 const SvgPhotoTeal = ({ rotation, ...props }: PhotoTealProps) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={300} height={270} fill="none" {...props}>
-    <g className="clipless-photo" style={{ transform: `rotate(${rotation}deg)` }}>
-      <g clipPath="url(#PhotoTeal_svg__a)">
-        <path fill="#FFFFFE" d="M0 30h300v240H0z" />
-        <path
-          fill="#E2F5F9"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M172 214c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v79c0 6.627-5.373 12-12 12h-82c-6.627 0-12-5.373-12-12z"
-        />
-        <path
-          fill="#10B9DF"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M172 214c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v16H172z"
-        />
-        <path fill="#353C51" d="M266 210h4l-9 12h-4z" />
-        <path fill="#353C51" d="M261 210h-4l9 12h4z" />
-        <path
-          fill="#E2F5F9"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M148 81c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v79c0 6.627-5.373 12-12 12h-82c-6.627 0-12-5.373-12-12z"
-        />
-        <path
-          fill="#10B9DF"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M148 81c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v16H148z"
-        />
-        <path fill="#353C51" d="M242 77h4l-9 12h-4z" />
-        <path fill="#353C51" d="M237 77h-4l9 12h4z" />
-        <path
-          fill="#353C51"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M2.582 1.043h158.421v203.188H2.582z"
-          transform="matrix(.91431 -.40501 .37675 .92631 -26.228 149.634)"
-        />
-        <path
-          fill="#10B9DF"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M2.582 1.043h158.421v203.188H2.582z"
-          transform="matrix(.91431 -.40501 .37675 .92631 -20.314 130.368)"
-        />
-        <path
-          fill="#FFFFFE"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M2.582 1.043H134.27v169.195H2.582z"
-          transform="matrix(.91431 -.40501 .37675 .92631 -2.078 140.968)"
-        />
-        <path
-          fill="#353C51"
-          d="M43.872 71.19c8.518-3.772 18.354.376 21.97 9.265a18.13 18.13 0 0 1-.083 13.86l24.13-5.364 16.619 23.009-78.044 34.571-4.062-28.571 20.251-14.256c-4.197-1.607-7.773-4.95-9.658-9.585-3.615-8.89.36-19.155 8.877-22.928m3.64 7.728c-4.188 1.856-6.143 6.904-4.365 11.275s6.616 6.412 10.805 4.557c4.189-1.856 6.144-6.905 4.366-11.277s-6.616-6.41-10.805-4.555"
-        />
-        <path
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M2.582 1.043h10.749v11.193H2.582z"
-          transform="matrix(.91431 -.40501 .37675 .92631 23.057 168.039)"
-        />
-        <path
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M2.582 1.043h10.749v11.193H2.582z"
-          transform="matrix(.91431 -.40501 .37675 .92631 34.002 194.949)"
-        />
-        <path
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M2.582 1.043h10.749v11.193H2.582z"
-          transform="matrix(.91431 -.40501 .37675 .92631 44.95 221.869)"
-        />
-        <path
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M2.582 1.043h10.749v11.193H2.582z"
-          transform="matrix(.91431 -.40501 .37675 .92631 55.897 248.783)"
-        />
-        <path
-          stroke="#353C51"
-          strokeWidth={4}
-          d="m49.516 171.876 72.565-32.145M60.46 198.795l72.567-32.145M71.41 225.706l72.566-32.145M82.357 252.625l72.566-32.144"
-        />
-        <path
-          fill="#E2F5F9"
-          stroke="#353C51"
-          strokeLinejoin="round"
-          strokeWidth={4}
-          d="M275.447 118.704 258.8 104.32l-88.917 102.906 16.647 14.384z"
-        />
-        <path
-          fill="#10B9DF"
-          stroke="#353C51"
-          strokeLinejoin="round"
-          strokeWidth={4}
-          d="M299.638 90.707c3.972-4.597 3.466-11.543-1.131-15.515s-11.543-3.465-15.515 1.131l-8.5 9.837 16.647 14.384z"
-        />
-        <path
-          fill="#FFFFFE"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M291.139 100.544 274.492 86.16l-15.69 18.16 16.646 14.384z"
-        />
-        <path
-          fill="#FFFFFE"
-          stroke="#353C51"
-          strokeLinejoin="round"
-          strokeWidth={4}
-          d="m186.529 221.61-16.646-14.384-14.56 33.675z"
-        />
-        <path fill="#353C51" d="m170.764 231.567-8.199-7.084-7.119 16.525z" />
-        <path
-          stroke="#353C51"
-          strokeWidth={4}
-          d="m285.152 105.944-15.543-13.43M279.922 111.997l-15.544-13.43M265.816 113.025l-87.609 101.393M2 32h296v236H2z"
-        />
-        <path
-          className="photo-clip"
-          fill="#646676"
-          stroke="#353C51"
-          strokeWidth={4}
-          d="M134 2h32l-4.571 48h-22.858z"
-        />
-      </g>
+  <svg
+    style={{ transform: `rotate(${rotation}deg)` }}
+    xmlns="http://www.w3.org/2000/svg"
+    width={300}
+    height={270}
+    fill="none"
+    {...props}
+  >
+    <g clipPath="url(#PhotoTeal_svg__a)">
+      <path fill="#FFFFFE" d="M0 30h300v240H0z" />
+      <path
+        fill="#E2F5F9"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M172 214c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v79c0 6.627-5.373 12-12 12h-82c-6.627 0-12-5.373-12-12z"
+      />
+      <path
+        fill="#10B9DF"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M172 214c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v16H172z"
+      />
+      <path fill="#353C51" d="M266 210h4l-9 12h-4z" />
+      <path fill="#353C51" d="M261 210h-4l9 12h4z" />
+      <path
+        fill="#E2F5F9"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M148 81c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v79c0 6.627-5.373 12-12 12h-82c-6.627 0-12-5.373-12-12z"
+      />
+      <path
+        fill="#10B9DF"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M148 81c0-6.627 5.373-12 12-12h82c6.627 0 12 5.373 12 12v16H148z"
+      />
+      <path fill="#353C51" d="M242 77h4l-9 12h-4z" />
+      <path fill="#353C51" d="M237 77h-4l9 12h4z" />
+      <path
+        fill="#353C51"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M2.582 1.043h158.421v203.188H2.582z"
+        transform="matrix(.91431 -.40501 .37675 .92631 -26.228 149.634)"
+      />
+      <path
+        fill="#10B9DF"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M2.582 1.043h158.421v203.188H2.582z"
+        transform="matrix(.91431 -.40501 .37675 .92631 -20.314 130.368)"
+      />
+      <path
+        fill="#FFFFFE"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M2.582 1.043H134.27v169.195H2.582z"
+        transform="matrix(.91431 -.40501 .37675 .92631 -2.078 140.968)"
+      />
+      <path
+        fill="#353C51"
+        d="M43.872 71.19c8.518-3.772 18.354.376 21.97 9.265a18.13 18.13 0 0 1-.083 13.86l24.13-5.364 16.619 23.009-78.044 34.571-4.062-28.571 20.251-14.256c-4.197-1.607-7.773-4.95-9.658-9.585-3.615-8.89.36-19.155 8.877-22.928m3.64 7.728c-4.188 1.856-6.143 6.904-4.365 11.275s6.616 6.412 10.805 4.557c4.189-1.856 6.144-6.905 4.366-11.277s-6.616-6.41-10.805-4.555"
+      />
+      <path
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M2.582 1.043h10.749v11.193H2.582z"
+        transform="matrix(.91431 -.40501 .37675 .92631 23.057 168.039)"
+      />
+      <path
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M2.582 1.043h10.749v11.193H2.582z"
+        transform="matrix(.91431 -.40501 .37675 .92631 34.002 194.949)"
+      />
+      <path
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M2.582 1.043h10.749v11.193H2.582z"
+        transform="matrix(.91431 -.40501 .37675 .92631 44.95 221.869)"
+      />
+      <path
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M2.582 1.043h10.749v11.193H2.582z"
+        transform="matrix(.91431 -.40501 .37675 .92631 55.897 248.783)"
+      />
+      <path
+        stroke="#353C51"
+        strokeWidth={4}
+        d="m49.516 171.876 72.565-32.145M60.46 198.795l72.567-32.145M71.41 225.706l72.566-32.145M82.357 252.625l72.566-32.144"
+      />
+      <path
+        fill="#E2F5F9"
+        stroke="#353C51"
+        strokeLinejoin="round"
+        strokeWidth={4}
+        d="M275.447 118.704 258.8 104.32l-88.917 102.906 16.647 14.384z"
+      />
+      <path
+        fill="#10B9DF"
+        stroke="#353C51"
+        strokeLinejoin="round"
+        strokeWidth={4}
+        d="M299.638 90.707c3.972-4.597 3.466-11.543-1.131-15.515s-11.543-3.465-15.515 1.131l-8.5 9.837 16.647 14.384z"
+      />
+      <path
+        fill="#FFFFFE"
+        stroke="#353C51"
+        strokeWidth={4}
+        d="M291.139 100.544 274.492 86.16l-15.69 18.16 16.646 14.384z"
+      />
+      <path
+        fill="#FFFFFE"
+        stroke="#353C51"
+        strokeLinejoin="round"
+        strokeWidth={4}
+        d="m186.529 221.61-16.646-14.384-14.56 33.675z"
+      />
+      <path fill="#353C51" d="m170.764 231.567-8.199-7.084-7.119 16.525z" />
+      <path
+        stroke="#353C51"
+        strokeWidth={4}
+        d="m285.152 105.944-15.543-13.43M279.922 111.997l-15.544-13.43M265.816 113.025l-87.609 101.393M2 32h296v236H2z"
+      />
+      <path fill="#646676" stroke="#353C51" strokeWidth={4} d="M134 2h32l-4.571 48h-22.858z" />
     </g>
     <defs>
       <clipPath id="PhotoTeal_svg__a">
--- src/components/ui/Pegboard/components/Sticky.tsx
+++ src/components/ui/Pegboard/components/Sticky.tsx
@@ -59,37 +59,30 @@ const BaseSticky = ({
       viewBox="0 0 221 271"
       fill="none"
       {...props}
-      style={{ overflow: 'visible' }}
+      style={{ overflow: 'visible', transform: `rotate(${rotation}deg)` }}
     >
-      <g style={{ overflow: 'visible' }}>
-        <g
-          className="clipless-sticky"
-          style={{ overflow: 'visible', transform: `rotate(${rotation}deg)` }}
-        >
-          <path
-            d="M218.645 36.7568H2.64507L2.64453 268.757H168.645L218.645 218.757V36.7568Z"
-            fill="#FFFFFE"
-            stroke="#353C51"
-            strokeWidth="4"
-            strokeMiterlimit="16"
-          />
-          <path
-            d="M168.645 268.757V218.757H218.645"
-            stroke="#353C51"
-            strokeWidth="4"
-            strokeMiterlimit="16"
-          />
-          <path d="M216.645 38.7568H4.64461L4.64453 65.7568H216.645V38.7568Z" fill={color} />
-          <path d="M216.645 65.7568H4.64453" stroke="#353C51" strokeWidth="4" />
-          {renderBodyText()}
-        </g>
-        <path
-          d="M94.6445 2.75684H126.645L122.073 50.7568H99.216L94.6445 2.75684Z"
-          fill="#646676"
-          stroke="#353C51"
-          strokeWidth="4"
-        />
-      </g>
+      <path
+        d="M218.645 36.7568H2.64507L2.64453 268.757H168.645L218.645 218.757V36.7568Z"
+        fill="#FFFFFE"
+        stroke="#353C51"
+        strokeWidth="4"
+        strokeMiterlimit="16"
+      />
+      <path
+        d="M168.645 268.757V218.757H218.645"
+        stroke="#353C51"
+        strokeWidth="4"
+        strokeMiterlimit="16"
+      />
+      <path d="M216.645 38.7568H4.64461L4.64453 65.7568H216.645V38.7568Z" fill={color} />
+      <path d="M216.645 65.7568H4.64453" stroke="#353C51" strokeWidth="4" />
+      {renderBodyText()}
+      <path
+        d="M94.6445 2.75684H126.645L122.073 50.7568H99.216L94.6445 2.75684Z"
+        fill="#646676"
+        stroke="#353C51"
+        strokeWidth="4"
+      />
       <defs>
         <clipPath id="clip0_8547_8398">
           <rect width="220" height="270" fill="white" transform="translate(0.644531 0.756836)" />
