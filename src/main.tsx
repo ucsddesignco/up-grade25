c8013bdc4891f0980f1499b06f675e09768f79ed
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2025-06-04 18:58:09 -0700

    fix: fix routing scroll

--- src/main.tsx
+++ src/main.tsx
@@ -2,30 +2,64 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './global.scss';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { createBrowserRouter, RouterProvider, ScrollRestoration, Outlet } from 'react-router';
 import RolesPage from './pages/RolesPage/RolesPage.tsx';
 import Quiz from './pages/Quiz/Quiz.tsx';
 import Result from './pages/ResultPage/Result.tsx';
 import Overview from './pages/Overview/Overview.tsx';
 import Navbar from './components/Navbar/Navbar.tsx';
 import FAQPage from './pages/FAQpage/FAQpage.tsx';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop.tsx';
+
+// eslint-disable-next-line react-refresh/only-export-components
+function RootLayout() {
+  return (
+    <>
+      <ScrollRestoration />
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <App />
+      },
+      {
+        path: 'overview',
+        element: <Overview />
+      },
+      {
+        path: 'quiz',
+        element: <Quiz />
+      },
+      {
+        path: 'faq',
+        element: <FAQPage />
+      },
+      {
+        path: 'roles',
+        element: <RolesPage />
+      },
+      {
+        path: 'result',
+        element: <Result />
+      },
+      {
+        path: 'roles/:roleId',
+        element: <Result />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ScrollToTop />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/overview" element={<Overview />} />
-        <Route path="/overview" element={<Overview />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/faq" element={<FAQPage />} />
-        <Route path="/roles" element={<RolesPage />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/roles/:roleId" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
