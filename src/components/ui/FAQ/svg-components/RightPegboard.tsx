212149bfa3de63f0b336e3c6a40a7bb920dcc6d2
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2025-06-04 17:37:32 -0700

    fix: fix build error

--- src/components/ui/FAQ/svg-components/RightPegboard.tsx
+++ src/components/ui/FAQ/svg-components/RightPegboard.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgRightPegboard = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={232} height={772} fill="none" {...props}>
