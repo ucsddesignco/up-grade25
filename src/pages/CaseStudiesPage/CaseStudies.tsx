7a6417a25af8dd0570b66da18ffb676fdbfcde32
pa1kina <p.volskaya@gmail.com>
2025-10-02 18:59:25 -0700

    lint fixes

--- src/pages/CaseStudiesPage/CaseStudies.tsx
+++ src/pages/CaseStudiesPage/CaseStudies.tsx
@@ -1,25 +1,25 @@
 import './CaseStudies.scss';
 import CaseStudiesCard from './CaseStudiesCard';
-import caseStudies from "./CaseStudiesData.json";
+import caseStudies from './CaseStudiesData.json';
 
 const CaseStudies: React.FC = () => {
   return (
-    <div className='case-studies-container'>
+    <div className="case-studies-container">
       <h2>Case Studies: 2025 Projects</h2>
-      <div className='case-studies'>
+      <div className="case-studies">
         {caseStudies.map((study, index) => (
-        <CaseStudiesCard
-          key={index}
-          projectName={study.projectName}
-          teamName={study.teamName}
-          imageSrc={study.imgSrc}
-          teamMembers={study.teamMembers}
-          documentationLink={study.documentationLink}
-        />
-      ))}
+          <CaseStudiesCard
+            key={index}
+            projectName={study.projectName}
+            teamName={study.teamName}
+            imageSrc={study.imgSrc}
+            teamMembers={study.teamMembers}
+            documentationLink={study.documentationLink}
+          />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default CaseStudies;
